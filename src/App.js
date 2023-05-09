773e6d67dcf293e9eaaf929caf50740195e4222c
yousseffdil <60134214+yousseffdil@users.noreply.github.com>
2023-05-09 17:10:31 +0200

    Finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import crashBandicootImage from './assets/crashBandicoot/CrashBandicoot.webp'
 import crashBandicootTittle from './assets/crashBandicoot/CrashLogo.webp'
 import CoDimage from './assets/COD/Pj.webp'
+import CotnrollerPS5 from './assets/PS5/Controller.webp'
+
 function App() {
   return (
     <div className="container">
@@ -13,6 +15,18 @@ function App() {
         <div className='Cod'>
           <img src={CoDimage} alt='CodPJ' className='CoDPJ'></img>
         </div>
+        <div className='PlayStation'></div>
+        <div className='Shop'></div>
+
+        <div className='RDR2'></div>
+
+        <div className='DMC5'></div>
+        <h1 className='TittlePS5'>PlayStation</h1>
+        <h1 className='TittlePS5'>PlayStation</h1>
+        <div className='PS5'>
+        <img src={CotnrollerPS5} alt='Controller' className='ControllerPS5'></img>
+        </div>
+
       </div>
     </div>
   );
